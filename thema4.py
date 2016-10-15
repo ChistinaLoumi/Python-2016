c60255a5de91a66dedb1a3f1d076c1a12c910bd5
ChistinaLoumi <christineloumi@gmail.com>
2016-10-15 15:58:38 +0300

    Update thema4.py

--- thema4.py
+++ thema4.py
@@ -1,11 +1,12 @@
 import urllib2
 import json
+#dinei o xrhsths thn tainia
 title=raw_input("Enter the title of the movie you are looking for:")
-x= "http://www.omdbapi.com/?t="+title+"&y=&plot=short&r=json"  
-url=x  
-request = urllib2.Request(url)
-handle = urllib2.urlopen(request)
-tainia = handle.read()
-
-print tainia 
-#PROSPATHISA NA TO XWRISW KAI NA PARV TA DEDOMENA ALLA DEN MOY DOYLEYE ME TIPOTA
+url= "http://www.omdbapi.com/?t="+title+"&y=&plot=short&r=json"  
+ #syndew thn istoselida me to programma
+response = urllib2.urlopen(url)
+#metatrepw ta stoixeia
+data = json.loads(response.read())
+#tatata...na to apotelesma
+print "the IMBD score is:"+data["imdbRating"]
+print "Awards:"+data["Awards"]
